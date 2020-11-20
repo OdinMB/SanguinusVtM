10219d9f33aa6a1664e4ad541f40e5035d923120
OdinMB <odin.muehlenbein@gmail.com>
2020-11-20 01:33:44 +0100

    wp for spending 1 WP

---
+++ src/commands/wp.js
@@ -0,0 +1,46 @@
+var Player = require("../models/player.js");
+var Character = require("../models/character.js");
+
+module.exports = {
+	name: 'wp',
+	description: 'Spends 1 WP with your selected character.',
+	oneline: true,
+	aliases: [],
+	usage: '[(opt) comment]',
+	cooldown: 6,
+	execute(message, args) {
+		Player.getPlayer(message, function (player) {
+			if (!player) return;
+
+			Character.findById(player.selectedCharacter).exec(function (err, character) {
+				if (err) {
+					console.log(err);
+					message.author.send(err.message);
+					return;
+				}
+				if (!character) {
+					message.author.send("You don't have a character selected.");
+					return;
+				}
+
+				if (character.WP < 1) {
+					message.reply("You have no WP left.");
+					return;
+				}
+
+				character.WP--;
+				character.save(function (err) {
+					if (err) {
+						console.log(err);
+						message.author.send(err.message);
+						return;
+					}
+
+					message.reply(character.name + " spent 1 WP" +
+						(args[0] ? " on '" + args.join(' ') + "'" : "") + ". " +
+						character.WP + "/" + character.WPMax + " WP left.");
+				});
+			});
+		});
+	}
+}
